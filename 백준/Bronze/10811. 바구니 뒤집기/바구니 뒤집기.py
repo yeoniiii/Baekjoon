8c98c74003ca5d5fff9c4a1607dd6f04936a5be9
HyeYeon Kim <sally879@naver.com>
2023-03-21 12:32:40 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -2,7 +2,5 @@ N, M = map(int, input().split())
 basket = [i+1 for i in range(N)]
 for r in range(M):
     i, j = map(int, input().split())
-    select = basket[(i-1):j]
-    select.reverse()
-    basket[(i-1):j] = select
+    basket[(i-1):j] = basket[(i-1):j][::-1]
 print(' '.join(map(str, basket)))
