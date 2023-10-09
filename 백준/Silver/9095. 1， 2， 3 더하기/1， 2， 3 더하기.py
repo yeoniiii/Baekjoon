4391b7a42b77b9d03557177cbf31d56b3413194f
HyeYeon Kim <sally879@naver.com>
2023-10-09 22:15:58 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,10 +1,7 @@
-num = [0]*10
-num[0] = 1
-num[1] = 2
-num[2] = 4
+num = [1,2,4]
 
 for i in range(3, 10):
-    num[i] = num[i-3] + num[i-2] + num[i-1]
+    num.append(num[i-3] + num[i-2] + num[i-1])
 
 T = int(input())
 for i in range(T):
