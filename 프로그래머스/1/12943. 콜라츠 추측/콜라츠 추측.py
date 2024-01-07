68570740f7a9809a764c22f4b4a7bdf03c387f6f
HyeYeon Kim <sally879@naver.com>
2024-01-08 01:18:17 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,11 +1,18 @@
 def solution(num):
     answer = 0
-    while num != 1:
-        if num % 2 == 0:
-            num /= 2
-        else:
-            num = num * 3 + 1
-        answer += 1
-        if answer == 500:
-            return -1
+    if num == 1:
+        pass
+    else:
+        while True:
+            if num == 1:
+                break
+            if answer >= 500:
+                answer = -1
+                break
+            elif num % 2 == 0:
+                num /= 2
+                answer += 1
+            else:
+                num = num * 3 + 1
+                answer += 1
     return answer
