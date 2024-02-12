b2e38f4a5750c5270b40f9f8a84afc722e139e1c
HyeYeon Kim <sally879@naver.com>
2024-02-12 15:49:36 +0900

    [Silver V] Title: 셀프 넘버, Time: 596 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -0,0 +1,11 @@
+arr = []
+for i in range(1, 10001):
+    i_sum = i
+    for j in range(len(str(i))):
+        i_sum += int(str(i)[j])
+    arr.append(i_sum)
+for i in range(1, 10001):
+    if i in arr:
+        continue
+    else:
+        print(i)
