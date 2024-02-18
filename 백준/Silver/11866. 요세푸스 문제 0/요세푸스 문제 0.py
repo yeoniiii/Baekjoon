8834b27703bfeb0e9a8f32c95127ab253ce5d965
HyeYeon Kim <sally879@naver.com>
2024-02-18 18:00:10 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 96 ms, Memory: 34008 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+N, K = map(int, input().split())
+
+queue = deque()
+for i in range(N):
+    queue.append(i+1)
+curr = queue[K-1]
+ans = '<'
+
+while queue:
+    if queue[0] == curr:
+        ans += str(queue[0])+', '
+        queue.popleft()
+        if len(queue) >= K:
+            curr = queue[K-1]
+        elif queue:
+            curr = queue[(K-1) % len(queue)]
+    else:
+        queue.append(queue.popleft())
+
+print(ans[:-2] + '>')
