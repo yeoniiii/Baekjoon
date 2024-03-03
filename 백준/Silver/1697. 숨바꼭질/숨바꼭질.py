04d3a33ba36fa79b0aeef6761cce63ea3248ea6b
HyeYeon Kim <sally879@naver.com>
2024-03-03 22:53:35 +0900

    [Silver I] Title: 숨바꼭질, Time: 100 ms, Memory: 34276 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,21 +1,21 @@
 from collections import deque
 
 N, K = map(int, input().split())
-sec = [0] * 100001
+visited = [0] * 100001
 
 def bfs(x):
     q = deque()
-    q.append(x)
+    q.append((x, 0))
+    visited[x] = 1
 
     while q:
-        v = q.popleft()
+        v, sec = q.popleft()
         if v == K:
-            return sec[v]
+            return sec
         else:
             for i in [v-1, v+1, v*2]:
-                if 0<=i<=100000 and sec[i] == 0:
-                    sec[i] = sec[v] + 1
-                    q.append(i)
-    return sec[v]
+                if 0<=i<=100000 and visited[i] == 0:
+                    q.append((i, sec+1))
+                    visited[i] = 1
 
 print(bfs(N))
