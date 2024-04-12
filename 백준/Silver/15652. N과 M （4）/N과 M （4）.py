d99c0f9a08f14f192c5323507712ca9f32fe2258
HyeYeon Kim <sally879@naver.com>
2024-04-12 11:50:48 +0900

    [Silver III] Title: N과 M (4), Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,18 @@
+N, M = map(int, input().split())
+visited = [0] * (N+1)
+arr = []
+
+def dfs():
+    if len(arr) == M:
+        print(' '.join(map(str, arr)))
+        return
+    for i in range(1, N+1):
+        if visited[i] < M:
+            if len(arr) == 0 or arr[-1] <= i:
+                visited[i] += 1
+                arr.append(i)
+                dfs()
+                visited[i] -= 1
+                arr.pop()
+            
+dfs()
