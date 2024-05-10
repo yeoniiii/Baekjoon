012fb49074d9e7ca81d5901cb7d93baabd9bad62
HyeYeon Kim <sally879@naver.com>
2024-05-10 12:43:55 +0900

    [Silver IV] Title: 정수 제곱근, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+l, r = 0, n
+min_n = n+1
+
+while l <= r:
+    mid = (l + r) / 2
+    if mid % 1 >= 0.5:
+        mid = int(mid) + 1
+    else:
+        mid = int(mid)
+
+    if mid**2 < n:
+        l = mid + 1
+    elif mid**2 > n:
+        r = mid - 1
+        min_n = min(min_n, mid)
+    else:
+        print(mid)
+        break
+
+if l > r:
+    print(min_n)
