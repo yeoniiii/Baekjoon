e821afb629f448ed1922dd9cff5f686fcee74a8c
HyeYeon Kim <sally879@naver.com>
2024-05-12 16:47:24 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 516 ms, Memory: 209852 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,11 +1,20 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
 s = input()
 q = int(input())
-for i in range(q):
+
+cumnum = {}
+for n in range(ord('a'), ord('z')+1):
+    a = chr(n)
+    cumnum[a] = [0]
+    for i in range(len(s)):
+        if s[i] == a:
+            cumnum[a].append(cumnum[a][i] + 1)
+        else:
+            cumnum[a].append(cumnum[a][i])
+
+for _ in range(q):
     a, l, r = input().split()
     l, r = int(l), int(r)
-    num, cur = 0, l
-    while cur <= r:
-        if s[cur] == a:
-            num += 1
-        cur += 1
-    print(num)
+    print(cumnum[a][r+1] - cumnum[a][l])
