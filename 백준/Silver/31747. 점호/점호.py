613f32918a69b6240a8d542e7de05f466b0a82e1
HyeYeon Kim <sally879@naver.com>
2024-06-08 13:21:32 +0900

    [Silver III] Title: 점호, Time: 124 ms, Memory: 32684 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/31747. 점호/점호.py
+++ 백준/Silver/31747. 점호/점호.py
@@ -1,27 +1,30 @@
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
-t = 0
 
+num = {1:0, 2:0}
+for a in A[:K]:
+    num[a] += 1
 if N == K:
-    num = {1:0, 2:0}
-    for a in A:
-        num[a] += 1
     print(max(num[1], num[2]))
 else:
-    while A:
-        a0 = A[0]
-        diff = 0
-        for i in range(min(K, len(A))):
-            if A[i] != a0:
-                diff = 1
-                del_i = i
-                break
-        if diff == 1:
-            A.pop(del_i)
-            A.pop(0)
-            t += 1
+    start = 0
+    t = 0
+    while start <= N:
+        if min(num[1], num[2]) == 0:
+            if num[1] == 0:
+                num[2] -= 1
+            else:
+                num[1] -= 1
+            if start+K < N:
+                num[A[start+K]] += 1
+            start += 1
         else:
-            A.pop(0)
-            t += 1
-
-    print(t)
+            num[1] -= 1
+            num[2] -= 1
+            if start+K < N:
+                num[A[start+K]] += 1
+            if start+K+1 < N:
+                num[A[start+K+1]] += 1
+            start += 2
+        t += 1
+    print(t-1)
