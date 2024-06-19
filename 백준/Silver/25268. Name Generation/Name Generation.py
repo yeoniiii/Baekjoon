e64a8bc7f5d06b952c90ef7e040ce66c2d33a47b
HyeYeon Kim <sally879@naver.com>
2024-06-19 10:44:12 +0900

    [Silver V] Title: Name Generation, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/25268. Name Generation/Name Generation.py
+++ 백준/Silver/25268. Name Generation/Name Generation.py
@@ -1,35 +1,15 @@
-import random
+C = "bcdfghjklmnpqrstvwxyz"
+V = "aeiou"
 
+def name(N):
+    for a in C:
+        for b in V:
+            for c in C:
+                for d in C:
+                    if N == 0:
+                        return 
+                    N -= 1
+                    print(a+b+c+d)
+        
 N = int(input())
-alphabets = []
-vowels = ['a', 'e', 'i', 'o', 'u']
-
-for i in range(ord('a'), ord('z')+1):
-    if chr(i) not in vowels:
-        alphabets.append(chr(i))
-
-S = set()
-
-while len(S) < N:
-    l = random.sample(range(3, 21), 1)
-    sample = []
-    while True:
-        if len(sample) >= l[0]:
-            break
-        sample += random.sample(alphabets, 1)
-        if len(sample) >= l[0]:
-            break
-        sample += random.sample(alphabets, 1)
-        if len(sample) >= l[0]:
-            break
-        sample += random.sample(vowels, 1)
-        if len(sample) >= l[0]:
-            break
-        sample += random.sample(vowels, 1)
-    name = ''.join(sample)
-    
-    if name not in S:
-        S.add(name)
-
-for s in S:
-    print(s)
+name(N)
