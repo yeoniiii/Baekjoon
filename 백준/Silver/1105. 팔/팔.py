b3ca77f9fc7d1068d497f441d38ae5cb3a60529b
HyeYeon Kim <sally879@naver.com>
2024-07-23 11:13:00 +0900

    [Silver I] Title: 팔, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/1105. 팔/팔.py
@@ -0,0 +1,12 @@
+L, R = input().split()
+if len(L) != len(R):
+    print(0)
+else:
+    ans = 0
+    for i in range(len(L)):
+        if L[i] == R[i]:
+            if L[i] == '8':
+                ans += 1
+        else:
+            break
+    print(ans)
