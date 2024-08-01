7e2baa96b00c29246b174d7a84d5a2551a763ff5
HyeYeon Kim <sally879@naver.com>
2024-08-01 15:11:24 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 2, Time: 1400 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Bronze/23882. 알고리즘 수업 － 선택 정렬 2/알고리즘 수업 － 선택 정렬 2.py
@@ -0,0 +1,13 @@
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+cnt = 0
+for n in range(N, 1, -1):
+    i = A.index(max(A[:n]))
+    if i != n-1:
+        A[i], A[n-1] = A[n-1], A[i]
+        cnt += 1
+        if cnt == K:
+            print(*A)
+            break
+if cnt < K:
+    print(-1)
