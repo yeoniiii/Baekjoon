15667e935284797ac42021884c1914065c0f5ba9
HyeYeon Kim <sally879@naver.com>
2024-09-04 09:32:22 +0900

    [Bronze III] Title: 보물 찾기, Time: 52 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/32068. 보물 찾기/보물 찾기.py
+++ 백준/Bronze/32068. 보물 찾기/보물 찾기.py
@@ -4,14 +4,9 @@ input = lambda: sys.stdin.readline().rstrip()
 T = int(input())
 for t in range(T):
     L, R, S = map(int, input().split())
-    x = 1
-    while True:
-        if x % 2 == 0:
-            S += x-1
-        else:
-            S -= x-1
-        if S == L or S == R:
-            break
-        x += 1
-    print(x)
-        
+    if abs(L-S) < abs(R-S):
+        dist = abs(L-S)
+        print(dist*2+1)
+    else:
+        dist = abs(R-S)
+        print(dist*2)
