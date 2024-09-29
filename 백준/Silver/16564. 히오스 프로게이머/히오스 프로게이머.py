5202c41532a6e1b4e1673f0a2097691cb154e594
HyeYeon Kim <sally879@naver.com>
2024-09-29 12:02:59 +0900

    [Silver I] Title: 히오스 프로게이머, Time: 164 ms, Memory: 34972 KB -BaekjoonHub

---
+++ 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.py
@@ -0,0 +1,26 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+N, K = map(int, input().split())
+X = []
+for _ in range(N):
+    X.append(int(input()))
+    
+def check(T):
+    s = 0
+    for x in X:
+        if x < T:
+            s += T-x
+    return s
+    
+l, r = min(X), max(X)+K
+while l <= r:
+    mid = (l+r)//2
+    if check(mid) <= K:
+        T = mid
+        if check(mid) == K:
+            break
+        l = mid+1
+    else:
+        r = mid-1
+print(T)
