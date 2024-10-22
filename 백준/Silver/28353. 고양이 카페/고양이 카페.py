0fb3af6efaf8f228f2d77fab773d3d358028c161
HyeYeon Kim <sally879@naver.com>
2024-10-22 10:36:20 +0900

    [Silver III] Title: 고양이 카페, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/28353. 고양이 카페/고양이 카페.py
+++ 백준/Silver/28353. 고양이 카페/고양이 카페.py
@@ -1,16 +1,14 @@
-n, k = map(int, input().split())
-data = list(map(int, input().split()))
-p1, p2 = 0, n - 1
+N, K = map(int, input().split())
+arr = sorted(list(map(int, input().split())))
+l, r = 0, N-1
 
-data.sort()
-answer = 0
-#p1이 작은쪽 p2가 큰쪽
-while p1 < p2:
-    if data[p1] + data[p2] <= k:
-        p1 += 1
-        p2 -= 1
-        answer += 1
+ans = 0
+while l < r:
+    if arr[l] + arr[r] <= K:
+        l += 1
+        r -= 1
+        ans += 1
     else:
-        p2 -= 1
+        r -= 1
     
-print(answer)
+print(ans)
