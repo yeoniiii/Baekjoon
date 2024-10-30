3b97244609ae1a3341a3b6e988a4a2195a1eec02
HyeYeon Kim <sally879@naver.com>
2024-10-30 12:15:47 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,15 @@
+nm = int(input())
+if nm < 425:
+    print("Violet")
+elif nm < 450:
+    print("Indigo")
+elif nm < 495:
+    print("Blue")
+elif nm < 570:
+    print("Green")
+elif nm < 590:
+    print("Yellow")
+elif nm < 620:
+    print("Orange")
+elif nm <= 780:
+    print("Red")
